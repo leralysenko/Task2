8a3175e83721d7424303f675060c4667c8b53edd
leralysenko <arellysenko@gmail.com>
2016-12-26 16:25:46 +0200

    second

---
+++ code.js
@@ -0,0 +1,27 @@
+
+  $('.carousel1').slick({
+    infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 3
+  });
+
+  $('.carousel2').slick({
+    infinite: true,
+  slidesToShow: 4,
+  slidesToScroll: 1
+  });
+
+  $('.carousel3').slick({
+    infinite: true,
+    dots: true,
+    arrows: false,
+    centerMode: true,
+    variableWidth: true
+  });
+
+
+  $('.carousel4').slick({
+    infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 3
+  });
